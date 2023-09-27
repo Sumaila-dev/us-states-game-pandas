602fecac39943018448bd70cce36bf2028e406fd
Abdul-Samed <82516089+Sumaila-dev@users.noreply.github.com>
2023-09-27 11:53:16 +0000

    final commit

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ all_states_data= data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct",prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct",prompt="What's another state's name? type 'exit' to quit game").title()
+    if answer_state == "Exit":
+        missing_states =[]
+        for state in all_states_data:
+            if state not in guessed_states:
+                missing_states.append(state)
+            missed_states_data = pandas.DataFrame(missing_states)
+            missed_states_data.to_csv("missed_states.csv")
+        turtle.bye()
+        break
+
     if answer_state in all_states_data:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -23,5 +33,5 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
         t.write(state_data.state.item())
 
-screen.exitonclick()
+#screen.exitonclick()
 
