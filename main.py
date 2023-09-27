ceb4d0758b9ce9e301c4be042efad6638fae60db
Abdul-Samed <82516089+Sumaila-dev@users.noreply.github.com>
2023-09-27 11:30:21 +0000

    main functionalities implemented

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import turtle
-
+import pandas
 screen = turtle.Screen()
 screen.title("US states Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-def get_mouse_click_coor(x,y):
-    print(x,y)
 
-turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
+data = pandas.read_csv("50_states.csv")
+all_states_data= data.state.to_list()
 
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct",prompt="What's another state's name?").title()
+    if answer_state in all_states_data:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+        t.write(state_data.state.item())
+
+screen.exitonclick()
 
